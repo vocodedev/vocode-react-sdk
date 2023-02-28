460f735e6d8ebe18bee2441b4a0b3ff085c939ae
Ajay Raj <ajay.n.raj@gmail.com>
2023-02-27 21:07:51 -0800

    export transcriber

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 export * from "./conversation";
 export * from "./vocode/agent";
 export * from "./vocode/audioEncoding";
+export * from "./vocode/transcriber";
 export * from "./vocode/synthesizer";
 export * from "./vocode/websocket";
