b4c34f2968a088e689bb133c1abf522863a45c83
Ajay Raj <ajay.n.raj@gmail.com>
2023-03-02 16:07:38 -0800

    add user implemented agent configs

--- src/types/vocode/agent.ts
+++ src/types/vocode/agent.ts
@@ -3,6 +3,7 @@ export type AgentType = "llm" | "echo";
 export interface AgentConfig {
   type: string;
   initialMessage?: string;
+  generateResponses?: boolean;
 }
 
 export interface LLMAgentConfig extends AgentConfig {
@@ -19,4 +20,23 @@ export interface ChatGPTAgentConfig extends AgentConfig {
 
 export interface EchoAgentConfig extends AgentConfig {
   type: "echo";
+}
+
+export interface RESTfulEndpointConfig {
+  url: string;
+  method?: "GET" | "POST";
+}
+
+export interface RESTfulUserImplementedAgentConfig extends AgentConfig {
+  type: "restful_user_implemented";
+  respond: RESTfulEndpointConfig;
+}
+
+export interface WebSocketRouteConfig {
+  url: string;
+}
+
+export interface WebSocketUserImplementedAgentConfig extends AgentConfig {
+  type: "websocket_user_implemented";
+  respond: WebSocketRouteConfig;
 }
