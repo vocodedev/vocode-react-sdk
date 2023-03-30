fa3dd9811a99c6ba0830395870bb93241326e938
Ajay Raj <ajay.n.raj@gmail.com>
2023-03-30 13:25:52 -0700

    adds play ht

--- src/types/vocode/synthesizer.ts
+++ src/types/vocode/synthesizer.ts
@@ -3,7 +3,9 @@ import { AudioEncoding } from "./audioEncoding";
 export type SynthesizerType =
   | "synthesizer_azure"
   | "synthesizer_google"
-  | "synthesizer_eleven_labs";
+  | "synthesizer_eleven_labs"
+  | "synthesizer_rime"
+  | "synthesizer_play_ht";
 
 export interface SynthesizerConfig {
   type: SynthesizerType;
@@ -21,7 +23,20 @@ export interface AzureSynthesizerConfig extends SynthesizerConfig {
 export interface GoogleSynthesizerConfig extends SynthesizerConfig {
   type: "synthesizer_google";
 }
+
 export interface ElevenLabsSynthesizerConfig extends SynthesizerConfig {
   type: "synthesizer_eleven_labs";
   voiceId?: string;
+}
+
+export interface RimeSynthesizerConfig extends SynthesizerConfig {
+  type: "synthesizer_rime";
+  speaker: string;
+}
+
+export interface PlayHtSynthesizerConfig extends SynthesizerConfig {
+  type: "synthesizer_play_ht";
+  voiceId: string;
+  speed?: string;
+  preset?: string;
 }
